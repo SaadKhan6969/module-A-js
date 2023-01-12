d3a0ad6a154c5be55ec71d520a83d1d59ff01e3c
SaadKhan6969 <115664292+SaadKhan6969@users.noreply.github.com>
2023-01-12 15:57:31 +0500

    Add files via upload

---
+++ js5/app.js
@@ -0,0 +1,723 @@
+// var a = "Karachi"
+// var b= prompt("Enter City")
+
+// a=a.toLowerCase()
+// b=b.toLowerCase()
+
+// a=a.toUpperCase()
+// b=b.toUpperCase()
+
+// var firstletter=a.slice(0,1)
+// var othersletter=a.slice(1)
+// a=firstletter.toUpperCase() +othersletter.toLowerCase()
+// console.log(a)
+
+// var firstletter=a.slice(0,1)
+// var othersletter=a.slice(1)
+// a=firstletter.toLowerCase()+othersletter.toUpperCase()
+// console.log(a)
+
+
+// if( a==b ){
+//     console.log("Karachiite")
+// }
+
+// var a = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cum vitae dolorem aspernatur aut fuga, adipisci excepturi amet optio sapiente deserunt ducimus aperiam saepe eaque quia laboriosam reprehenderit? Minima, commodi amet nihil!"
+// var b = "amet"
+// for (i = 0; i < a.length; i++) {
+//     var word = a.slice(i, i + 4)
+//     if (word == b) {
+//         console.log("Found", word)
+//         console.log(i)
+//         var startingletters = a.slice(0, i)
+//         var myword = "Ametment"
+//         var otherletters = a.slice(i + 4)
+//         a = startingletters + myword + otherletters
+//     }
+// }
+
+// var a=a.replace(/amet/g,"ametment")
+// console.log(a)
+
+// var ab =a.charAt()
+// console.log(ab)
+
+
+// var a=Math.round(1.9)
+// var a=Math.floor(1.9)
+// var a=Math.ceil(1.2)
+
+
+// var a=1.288999
+// a= a.toFixed(3)
+
+
+// var a="abc"
+// a=+a
+// a=Number(a)
+// a= parseInt(a)
+// a= parseFloat(a)
+// var type=typeof a
+// console.log(a)
+// console.log(type)
+
+// var a=10
+// a= a.toString()
+// var type= typeof(a)
+// console.log(a)
+// console.log(type)
+
+
+
+// var a="Lorem ipsum dolor sit, amet consectetur amet adipisicing elit. Doloremque autem sapiente impedit itaque adipisci perspiciatis ducimus, accusantium sunt at enim dolor expedita amet repudiandae tenetur accusamus nostrum magni voluptate dolor cupiditate tempore officia aliquam quae. Aliquid fuga natus veniam commodi neque minus amet sint, aut recusandae, impedit, ducimus molestias accusantium hic amet laudantium et vero?"
+// var b="dolor"
+// var i=a.indexOf("dolor")
+// for (i = 0; i < a.length; i++){
+//     var word=a.slice(i,i+5)
+//     if(b==word){
+//         console.log(word,"found")
+//         console.log(i)
+//     }
+// }
+// var a=a.replace(/dolor/g,"huehuehue")
+// console.log(a)
+
+// var abc=a.charAt(34)
+// console.log(abc)
+
+// var a=Math.round(2.23)
+// var a=Math.floor(2.9)
+// var a =Math.ceil(1.1)
+// console.log(a)
+
+
+// var a=Math.random()
+// var hue=Math.round(a)
+// var huehue=Math.ceil(a*6)
+// console.log(huehue)
+
+// var months=["jan","Feb","Mar","Apr","May","Jun","Jul","Aug","sept","Oct","nov","Dec"]
+
+// var dt=new Date()
+// var a=dt.getMonth()
+// var a=dt.getMonth()
+// var a=dt.getDay()
+// var a=dt.getDate()
+// var a=dt.getFullYear()
+// var a=dt.getHours()
+// var a=dt.getMinutes()
+// var a=dt.getSeconds()
+// var a=dt.getMilliseconds()
+// dt.setDate(17)
+// dt.setMonth(5)
+// dt.setFullYear(2004)
+// dt.setHours(00)
+// dt.setMinutes(00)
+// dt.setSeconds(00)
+
+
+
+// console.log(dt)
+// console.log(months[a])
+
+
+// var vv=new Date("6-17-2000")
+// var dt2=new Date()
+// vv.setMonth(9)
+// vv.setDate(23)
+// vv.setFullYear(2000)
+// vv.setHours(00)
+// vv.setMinutes(00)
+// vv.setSeconds(00)
+// var a=vv.getTime()
+// var b=dt2.getTime()
+// var c=b-a
+// var d=Math.round(c/(1000*60*60*24*365))
+
+// console.log(a)
+// console.log(b)
+// console.log(c/(1000*60*60*24*365))
+// console.log(d)
+
+// function abc (){
+//     console.log("huehue")
+//     alert("2jz")
+// }
+
+// abc()
+
+// function calculateDiscount(a, b){
+//     var amount =a
+//     var discount=b
+//     var totalamount=amount-discount
+//     console.log(totalamount)
+// }
+// calculateDiscount(100,20)
+// calculateDiscount(200,40)
+// calculateDiscount(300,60)
+// calculateDiscount(400,80)
+// calculateDiscount(500,100)
+
+// function abc(a,b){
+//     var amount=a
+//     var discount=b
+//     var totalamount=a-b
+//     console.log(totalamount)
+// }
+// abc(100,20)
+// abc(500,100)
+// abc(1000,200)
+// abc(5000,300)
+// abc(10000,400)
+
+// var a="xyz"
+
+// function abc(){
+//     console.log(a)
+// }
+// abc()
+
+
+// var count=0
+// function addcount(){
+//     count=1
+// }
+// addcount()
+// console.log(count)
+
+
+// function abc(){
+//     var a=10
+//     return a
+// }
+// var c=abc()
+// console.log(c)
+
+
+
+// function abc(){
+//     var a="xbc"
+// }
+// console.log(a)
+// abc()
+// var count=0
+// function addcount(){
+//     count=1
+// }
+
+// addcount()
+// console.log(count)
+
+
+// function xyz(){
+//     var a=10
+//     return a
+// }
+// var c=xyz()
+// console.log(c)
+
+// var day="mon"
+
+// switch(day){
+//     case "sun":
+//         console.log("Holiday");
+//         break;
+//     case "mon":
+//         console.log("First Day Of Week");
+//         break;
+//       case "tue":
+//         console.log("Second Day Of Week")
+//         break;
+//         default:
+//             console.log("no Day Found")
+// }
+
+// var day="sat"
+// switch(day){
+//     case "sun":
+//         alert("Holiday");
+//         break;
+//         case "mon":
+//             alert("First Day Of Week");
+//             break;
+//             case "tue":
+//                 alert("Second Day of Week")
+//                 break;
+//                 default:
+//                     alert("Day Not Found")
+// }
+
+// While Loop Start.....
+
+// var i=0
+// while(i<10){
+//     i++
+//     console.log(i)
+// }
+// While Loop end.....
+
+
+
+// Do While Loop Strt
+// var i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<1)
+// Do While Loop End
+
+
+// var i=0
+// while(i<10){
+//     i++
+//     console.log(i)
+// }
+
+// var i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<1)
+
+// var i=0
+// while(i<10){
+//     i++
+//     console.log(i)
+// }
+
+
+// var i=0
+// do{
+//     console.log(i)
+//     i++
+// }while(i<1)
+
+// function abc(a){
+//     console.log(a)
+// }
+
+
+// function calc () {
+//     var a = +prompt("Enter a num")
+//     var b = +prompt("Enter a num")
+//     var c = prompt("Enter a opr")
+//     if (c === "+"){
+//         console.log(a+b)
+//     }
+//     else if (c === "-"){
+//         console.log(a-b)
+//     }
+//     else if (c === "*"){
+//         console.log(a*b)
+//     }
+//     else if (c === "/"){
+//         console.log(a/b)
+//     }
+// }
+// calc()
+
+// function square() {
+//     var a = +prompt("Enter a num")
+//     var b = a**2
+//     console.log(b)
+
+// }
+// square()
+
+// function counting(){
+//     var a=+prompt("Enter Starting Num")
+//     var b=+prompt("Enter Ending Num")
+//     for(i=a;i<=b;i++){
+//         console.log(i)
+//     }
+// }
+// counting()
+
+// function abc(a){
+//     console.log(a)
+// }
+
+
+// var textinput=document.getElementById("hue")
+
+// function getvalue(){
+//     console.log(textinput.value)
+// }
+
+
+// var text =document.getElementById("hue")
+// var error=document.getElementById("er")
+// var a=[]
+// function addvalue(){
+// var b=text.value
+// if(b == ""){
+//     error.value ="Enter Value"
+// }else{
+//     a.push(b)
+//     console.log(a)
+// }
+
+    // arr.push(text.value)
+    // console.log(a)
+    // console.log(text.value)
+    
+// }
+
+// var text=document.getElementById("huehue")
+// var errorpera=document.getElementById("er")
+// var arrays=[]
+// function advalue(){
+//     var b=text.value
+//     if(b==""){
+//         errorpera.innerHTML="Enter Value"
+//     }else{
+//         arrays.push(b)
+//         console.log(arrays)
+//     }
+// }
+
+// function abc(a){
+//     a.preventDefault()
+//     console.log("Hello WOrld")
+// }
+
+// var tex=document.getElementById("tx")
+// var value=document.getElementById("er")
+// function but(){
+// var a=tex.value
+// if(a==""){
+//     value.value="Enter Value"
+// }else{
+//     console.log(a)
+// }
+// }
+
+// function abc(a){
+// console.log(a)
+// a.src="https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320"
+// }
+// function ab(a){
+//     console.log(a)
+//     a.src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+// }
+// function xyz(a){
+// console.log(a)
+// // a.className +="redbox"
+// a.style.display="none"
+// a.style.backgroundColor="green"
+// }
+// var a=document.getElementById("box")
+// function shapes(cls){
+//     console.log(a)
+//     a.className=cls
+
+// }
+
+// var parent=document.getElementById('parent')
+// var a=parent.getElementsByTagName("p")
+// for(var i=0;i<a.length;i++){
+//     a[i].className="colors"
+// }
+// console.log(a)
+
+
+
+// var parent = document.getElementById('imgparent')
+
+// var allimages  = parent.getElementsByTagName('img')
+
+// var showslide = document.getElementById('showslide');
+
+// var indexno = 0;
+
+
+
+// function renderslide(){
+
+// showslide.src = allimages[indexno].src;
+
+
+// }
+
+
+
+// renderslide();
+
+
+// function nextslide(){
+    
+
+//     if(indexno+1 == allimages.length){
+
+//         indexno = 0;
+//     }
+
+//     else{
+
+//         indexno++;
+//     }
+//     renderslide();
+
+
+// }
+
+
+
+
+// function prevslide(){
+
+
+
+//     if(indexno == 0){
+
+//         indexno = allimages.length-1
+//     }
+// else{    indexno--;}
+//     renderslide();
+
+
+// }
+
+
+// var mn=document.getElementById('mn')
+// function addval(){
+//     var para=document.createElement('p')
+//     var paratext=document.createTextNode("PARAGRAPH huehue")
+//     para.appendChild(paratext)
+//     mn.appendChild(para)
+// }
+
+
+
+// object2 strt
+// function abc(name,age,rollno){
+//     var a={
+//         name:name,
+//         age:age,
+//         rollno:rollno,
+//     }
+//     console.log(a)
+// }
+// abc("saad",18,1)
+
+
+// function Abc(name,age,rollno){
+//     this.name=name;
+//     this.age=age;
+//    this.rollno=rollno;
+// }
+// Abc.prototype.inst="Saims"
+// var obj=new Abc("Saad",18,1)
+// var obj1=new Abc("fahad",19,2)
+// console.log(obj)
+// console.log(obj1)
+
+
+// object2 end
+
+
+// Todo App Strt
+
+
+// var main=document.getElementById('mn')
+// function addvalue(){
+//     if(inp.value==""){
+//         alert("Enter Something")
+//     }else{
+//         var li=document.createElement('li')
+//         var litext=document.createTextNode(inp.value)
+//         li.appendChild(litext)
+//         main.appendChild(li)
+//         inp.value=""
+        
+
+//         var editbtn=document.createElement('BUTTON')
+//         var editbtntext=document.createTextNode('edit')
+//         editbtn.appendChild(editbtntext)
+//         editbtn.setAttribute("onclick","edit(this)")
+//         li.appendChild(editbtn)
+//         main.appendChild(li)
+//         console.log(inp.value)
+
+//         var delbtn=document.createElement('BUTTON')
+//         var delbtntext=document.createTextNode('del')
+//         delbtn.appendChild(delbtntext)
+//         delbtn.setAttribute("onclick","del(this)")
+//         li.appendChild(delbtn)
+//         main.appendChild(li)
+        
+//     }
+// }
+
+// function edit(element){
+//     var newvalue=prompt("ENter NEw Value")
+//    console.log( element.parentNode.firstChild.nodeValue
+//     )
+//     element.parentNode.firstChild.nodeValue=newvalue
+
+// }
+// function del(element){
+//     element.parentNode.remove()
+// }
+// function delvalue(){
+//     main.innerHTML=""
+//     inp.value=""
+// }
+
+
+// Todo App ENd
+
+// object strt
+// var students=[
+//     {
+//         name:"saad",
+//         rollno:1,
+//         age:18,
+//         inst:"saims",
+//         },
+//     {
+//         name:"Hamdan",
+//         rollno:2,
+//         age:19,
+//         inst:"bck",
+//         },
+//     {
+//         name:"soban",
+//         rollno:3,
+//         age:18,
+//         inst:"snfc",
+//         },
+// ]
+
+// var inp=document.getElementById('inp')
+// function add(){
+// for(i=0;i<students.length;i++){
+//     if(inp.value==students[i].rollno){
+//         console.log(students[i])
+//     }
+// }
+// }
+
+
+// var inp=document.getElementById('inp')
+// var roll=document.getElementById('rol')
+// var age=document.getElementById('age')
+// var inst=document.getElementById('ins')
+// var nm=document.getElementById('nm')
+// function add(){
+//     for(i=0;i<students.length;i++){
+//         if(inp.value==(students[i]).rollno){
+//             nm.innerHTML=(students[i]).name
+//             roll.innerHTML=(students[i]).rollno
+//             inst.innerHTML=(students[i]).inst
+//             age.innerHTML=(students[i]).age
+//               }
+//     }
+// }
+
+
+// var inp=document.getElementById('inp')
+// var nam=document.getElementById('nm')
+// var roll=document.getElementById('rol')
+// var age=document.getElementById('age')
+// var inst=document.getElementById('ins')
+// function add(){
+//  for(i=0;i<students.length;i++){
+//     if(inp.value==students[i].rollno){
+//         nam.innerHTML=students[i].name
+//         roll.innerHTML=students[i].rollno
+//         age.innerHTML=students[i].age
+//         inst.innerHTML=students[i].inst
+//     }
+//  }
+
+// }
+
+// var firstname=document.getElementById('firstname')
+// var lastname=document.getElementById('lastname')
+// var email=document.getElementById('email')
+// var obj={}
+// function submit(){
+//     obj.firstname=firstname.value
+//     obj.lastname=lastname.value
+//     obj.email=email.value
+// }
+// console.log(obj)
+
+
+
+// object end
+
+
+// // stop wtch srt
+
+// var displaymillisec=document.getElementById('displaymillisec')
+// var displaysec=document.getElementById('displaysec')
+// var displaymin=document.getElementById('displaymin')
+// millisec=0;
+// sec=0;
+// min=0;
+// var interval;
+
+// function timer(){
+//     millisec++
+//     if(millisec == 10){
+//         millisec= 0
+//         sec++
+//         if(sec == 60){
+//             sec=0
+//             min++
+//         }
+//     }
+//     displaymillisec.innerHTML=millisec
+//     displaysec.innerHTML=sec
+//     displaymin.innerHTML=min
+// }
+
+// function starttimer(){
+//     interval=setInterval(function(){
+//         timer()
+//     }
+//     ,100
+//     )
+// }
+
+
+
+// function pausetimer(){
+//     clearInterval(interval)
+// }
+
+// function resettimer(){
+//     pausetimer()
+//     millisec=0;
+//     sec=0;
+//     min=0;
+//     displaymillisec.innerHTML=millisec
+//     displaysec.innerHTML=sec
+//     displaymin.innerHTML=min
+// }
+
+
+// calc strt
+var display=document.getElementById('inp')
+function getvalue(a){
+    display.value +=a
+}
+function equ(){
+    a=eval(display.value)
+    display.value=a
+}
+function clrvalue(){
+    display.value=""
+}
+
+
+
+
+
+
+
+
+
+// calc end
